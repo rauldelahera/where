2d4bf2506015d8ed6f829fae119b67c10f751711
RumenKanev <rkynev@gmail.com>
2021-12-08 16:33:49 +0000

    new map

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Map from "./components/Map";
+import Form from "./components/Form";
+
+export default function App() {
+  return (
+    <>
+      <Form />
+      <Map />
+    </>
+  );
+}
